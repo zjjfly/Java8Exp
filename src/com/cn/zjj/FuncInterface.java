c0957360a86c02572250509a320caf836e42e127
zjjfly <zjjblue@126.com>
2014-09-22 21:00:48 +0800

    3
    1

--- src/com/cn/zjj/FuncInterface.java
+++ src/com/cn/zjj/FuncInterface.java
@@ -9,6 +9,6 @@ public class FuncInterface {
 	public static void main(String[] args) {
 		Converter<String, Integer> converter = (from) -> Integer.valueOf(from);
 		Integer converted = converter.convert("123");
-		System.out.println(converted); // 123
+		System.out.println(converted); 
 	}
 }
