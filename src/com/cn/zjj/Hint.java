6be818502e9b5f354dfcc630c87c4f9b3f222e64
zjjfly <zjjblue@126.com>
2015-02-23 01:11:31 +0800

    correct something wrong

--- src/com/cn/zjj/Hint.java
+++ src/com/cn/zjj/Hint.java
@@ -1,7 +1,12 @@
 package com.cn.zjj;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Repeatable;
-
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+@Retention(RetentionPolicy.RUNTIME) 
+@Target(ElementType.TYPE)
 @Repeatable(Hints.class) 
 @interface Hint {
     String value();
--- src/com/cn/zjj/HintUser.java
+++ src/com/cn/zjj/HintUser.java
@@ -1,4 +1,5 @@
 package com.cn.zjj;
+
 @Hint("hint1")
 @Hint("hint2")
  interface HintUser {
