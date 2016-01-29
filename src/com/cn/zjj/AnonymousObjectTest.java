a62837ac35eb299db25676639169e874aa7b344b
zjjfly <zjjblue@gmail.com>
2016-01-29 15:00:28 +0800

    匿名对象

---
+++ src/com/cn/zjj/AnonymousObjectTest.java
@@ -0,0 +1,23 @@
+package com.cn.zjj;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by zjjfly on 16/1/27.
+ */
+public class AnonymousObjectTest{
+    public static void main(String[] args) throws MalformedURLException {
+        new Object(){
+            void dos(){
+                System.out.println("阿达");
+            }
+
+        }.dos();
+        http://www.baidu.com
+        System.out.println("");
+
+    }
+
+
+}
